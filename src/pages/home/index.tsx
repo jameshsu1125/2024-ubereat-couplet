b031169c039e03968ceb56d2289c03e905966036
jameshsu1125 <jameshsu1125@gmail.com>
2024-11-12 21:06:50 +0800

    update

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,22 +1,47 @@
-import Button from '@/components/button';
-import useTodos from '@/hooks/useTodos';
-import { IReactProps } from '@/settings/type';
-import { memo, useState } from 'react';
+import { memo, useEffect, useState } from 'react';
 import { HomeContext, HomeState, THomeState } from './config';
 import './index.less';
 
-const Home = memo(({ children }: IReactProps) => {
+const Home = memo(() => {
   const [state, setState] = useState<THomeState>(HomeState);
-  const [todos, getTodos] = useTodos();
+
+  useEffect(() => {
+    const button = document.querySelector('.shuffle');
+    const grid = document.querySelector('.grid');
+
+    if (button && grid) {
+      button.addEventListener('click', () => {
+        for (var i = grid.children.length; i >= 0; i--) {
+          grid.appendChild(grid.children[(Math.random() * i) | 0]);
+        }
+        [...grid.children].forEach((e) => {
+          console.log(e.getBoundingClientRect(), e);
+        });
+      });
+    }
+  }, []);
 
   return (
     <div className='Home'>
       <HomeContext.Provider value={[state, setState]}>
-        <h1 className='text-2xl'>{children}</h1>
-        <Button onClick={getTodos}>
-          <Button.regular>Fetch</Button.regular>
-        </Button>
-        <p className='text-center'>{JSON.stringify(todos)}</p>
+        <div className='shuffleWrap'>
+          <button className='shuffle'>Shuffle order of boxes</button>
+        </div>
+        <div className='grid'>
+          <div className='box blue wide'></div>
+          <div className='box green square'></div>
+          <div className='box green square'></div>
+          <div className='box green tall'></div>
+          <div className='box red square'></div>
+          <div className='box red square'></div>
+          <div className='box red square'></div>
+          <div className='box green square'></div>
+          <div className='box red wide'></div>
+          <div className='box blue square'></div>
+          <div className='box blue tall'></div>
+          <div className='box green wide'></div>
+          <div className='box red square'></div>
+        </div>
       </HomeContext.Provider>
     </div>
   );
